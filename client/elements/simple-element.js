0c1a61a05c7a224b54dd2ec61ffdab8fbeebc1ac
Saša Mitrović <sasha.mitrovich@marklogic.com>
2015-11-12 14:59:48 +0100

    fixed styling and focusing to improve the user experience when using only the keyboard

--- client/elements/simple-element.js
+++ client/elements/simple-element.js
@@ -15,7 +15,7 @@ Polymer({
     guess.guessedValue=this.value;
     guess.generatedNumberPair=this.randomNumbersPair;
     this.hasAnswered = true;
-
+    document.getElementById("replay").focus();
     //supplying an array of values (in case you have multiple arguments in the uservice method)
     this.notifyPath('guess', [guess]);
   },
@@ -24,24 +24,34 @@ Polymer({
     this.hasAnswered=false;
     this.response=null;
     this.value=null
-    this.notifyPath('focus',true);
+    document.getElementById("edit").$.input.focus();
   },
   ready: function () {
     this.notifyPath('trigger',[]);
+    this.response=null;
+    document.getElementById("edit").$.input.focus();
   },
-  observers: [
-    'onAnswer(answer)'
-  ],
-  onAnswer: function () {
-    var response="";
-    response=+this.answer.value+" is the correct answer"
 
-    if (this.answer.isCorrect) {
-      response="Congrats, "+response+"!";
+  // check if the user pressed the 'enter' to submit their answer
+  keyPressed: function(e) {
+    if (    0 === e.charCode
+      && !this.hasAnswered) {
+        this.submitAnswer(e);
+      }
+    },
+    observers: [
+      'onAnswer(answer)'
+    ],
+    onAnswer: function () {
+      var response="";
+      response=+this.answer.value+" is the correct answer"
+
+      if (this.answer.isCorrect) {
+        response="Congrats, "+response+"!";
+      }
+      else {
+        response="Sorry, "+response+" :(";
+      }
+      this.notifyPath('response', [response]);
     }
-    else {
-      response="Sorry, "+response+" :(";
-    }
-    this.notifyPath('response', [response]);
-  }
-});
+  });
